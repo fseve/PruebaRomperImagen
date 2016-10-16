a501e3b840b178150c9561e5124ebc1942a05d67
fseve <falcu2345@gmail.com>
2016-10-15 23:30:42 -0500

    Agregar archivos de proyecto.

---
+++ PruebaRomperImagen/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace PruebaRomperImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtImagen = new System.Windows.Forms.TextBox();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.btnRomper = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic0 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic10 = new System.Windows.Forms.PictureBox();
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic11 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic15 = new System.Windows.Forms.PictureBox();
+            this.pic12 = new System.Windows.Forms.PictureBox();
+            this.pic14 = new System.Windows.Forms.PictureBox();
+            this.pic13 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imagen";
+            // 
+            // txtImagen
+            // 
+            this.txtImagen.Location = new System.Drawing.Point(60, 6);
+            this.txtImagen.Name = "txtImagen";
+            this.txtImagen.Size = new System.Drawing.Size(264, 20);
+            this.txtImagen.TabIndex = 1;
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(341, 4);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(75, 23);
+            this.btnExaminar.TabIndex = 2;
+            this.btnExaminar.Text = "Seleccionar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+            // 
+            // picMain
+            // 
+            this.picMain.Location = new System.Drawing.Point(12, 47);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(404, 441);
+            this.picMain.TabIndex = 4;
+            this.picMain.TabStop = false;
+            this.picMain.Visible = false;
+            // 
+            // btnRomper
+            // 
+            this.btnRomper.Location = new System.Drawing.Point(422, 465);
+            this.btnRomper.Name = "btnRomper";
+            this.btnRomper.Size = new System.Drawing.Size(75, 23);
+            this.btnRomper.TabIndex = 5;
+            this.btnRomper.Text = "Romper";
+            this.btnRomper.UseVisualStyleBackColor = true;
+            this.btnRomper.Click += new System.EventHandler(this.btnRomper_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(552, 47);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(100, 95);
+            this.pic1.TabIndex = 6;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(658, 47);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(100, 95);
+            this.pic2.TabIndex = 7;
+            this.pic2.TabStop = false;
+            // 
+            // pic0
+            // 
+            this.pic0.Location = new System.Drawing.Point(446, 47);
+            this.pic0.Name = "pic0";
+            this.pic0.Size = new System.Drawing.Size(100, 95);
+            this.pic0.TabIndex = 8;
+            this.pic0.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(446, 148);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(100, 95);
+            this.pic4.TabIndex = 11;
+            this.pic4.TabStop = false;
+            // 
+            // pic6
+            // 
+            this.pic6.Location = new System.Drawing.Point(658, 148);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(100, 95);
+            this.pic6.TabIndex = 10;
+            this.pic6.TabStop = false;
+            // 
+            // pic5
+            // 
+            this.pic5.Location = new System.Drawing.Point(552, 148);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(100, 95);
+            this.pic5.TabIndex = 9;
+            this.pic5.TabStop = false;
+            // 
+            // pic8
+            // 
+            this.pic8.Location = new System.Drawing.Point(446, 249);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(100, 95);
+            this.pic8.TabIndex = 14;
+            this.pic8.TabStop = false;
+            // 
+            // pic10
+            // 
+            this.pic10.Location = new System.Drawing.Point(658, 249);
+            this.pic10.Name = "pic10";
+            this.pic10.Size = new System.Drawing.Size(100, 95);
+            this.pic10.TabIndex = 13;
+            this.pic10.TabStop = false;
+            // 
+            // pic9
+            // 
+            this.pic9.Location = new System.Drawing.Point(552, 249);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(100, 95);
+            this.pic9.TabIndex = 12;
+            this.pic9.TabStop = false;
+            // 
+            // pic11
+            // 
+            this.pic11.Location = new System.Drawing.Point(764, 249);
+            this.pic11.Name = "pic11";
+            this.pic11.Size = new System.Drawing.Size(100, 95);
+            this.pic11.TabIndex = 17;
+            this.pic11.TabStop = false;
+            // 
+            // pic7
+            // 
+            this.pic7.Location = new System.Drawing.Point(764, 148);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(100, 95);
+            this.pic7.TabIndex = 16;
+            this.pic7.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(764, 47);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(100, 95);
+            this.pic3.TabIndex = 15;
+            this.pic3.TabStop = false;
+            // 
+            // pic15
+            // 
+            this.pic15.Location = new System.Drawing.Point(764, 350);
+            this.pic15.Name = "pic15";
+            this.pic15.Size = new System.Drawing.Size(100, 95);
+            this.pic15.TabIndex = 21;
+            this.pic15.TabStop = false;
+            // 
+            // pic12
+            // 
+            this.pic12.Location = new System.Drawing.Point(446, 350);
+            this.pic12.Name = "pic12";
+            this.pic12.Size = new System.Drawing.Size(100, 95);
+            this.pic12.TabIndex = 20;
+            this.pic12.TabStop = false;
+            // 
+            // pic14
+            // 
+            this.pic14.Location = new System.Drawing.Point(658, 350);
+            this.pic14.Name = "pic14";
+            this.pic14.Size = new System.Drawing.Size(100, 95);
+            this.pic14.TabIndex = 19;
+            this.pic14.TabStop = false;
+            // 
+            // pic13
+            // 
+            this.pic13.Location = new System.Drawing.Point(552, 350);
+            this.pic13.Name = "pic13";
+            this.pic13.Size = new System.Drawing.Size(100, 95);
+            this.pic13.TabIndex = 18;
+            this.pic13.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(527, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic15);
+            this.Controls.Add(this.pic12);
+            this.Controls.Add(this.pic14);
+            this.Controls.Add(this.pic13);
+            this.Controls.Add(this.pic11);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic10);
+            this.Controls.Add(this.pic9);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic0);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.btnRomper);
+            this.Controls.Add(this.picMain);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.txtImagen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtImagen;
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Button btnRomper;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic0;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic10;
+        private System.Windows.Forms.PictureBox pic9;
+        private System.Windows.Forms.PictureBox pic11;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic15;
+        private System.Windows.Forms.PictureBox pic12;
+        private System.Windows.Forms.PictureBox pic14;
+        private System.Windows.Forms.PictureBox pic13;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PruebaRomperImagen/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaRomperImagen
+{
+    public partial class Form1 : Form
+    {
+
+        //List<int> vec = new List<int>();
+        int[] vec = new int[16];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExaminar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.Title = "Seleccionar imagen";
+            oFD.Filter = "Imágenes|*.jpg;*.gif;*.png;*.bmp|Todos (*.*)|*.*";
+            oFD.FileName = this.txtImagen.Text;
+            if (oFD.ShowDialog() == DialogResult.OK)
+            {
+                txtImagen.Text = oFD.FileName;
+                picMain.Image = Image.FromFile(txtImagen.Text);
+                picMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnRomper_Click(object sender, EventArgs e)
+        {
+            LlenarVector();
+
+            // Trocear una imagen en trozos más pequeños
+            const int columnas = 4;
+            const int filas = 4;
+            //
+            // El tamaño proporcional del ancho y alto
+            // correspondientes a los trozos a usar
+            int tamTrozoW = picMain.Image.Width / columnas;
+            int tamTrozoH = picMain.Image.Height / filas;
+            // El tamaño de cada trozo
+            int nW = pic0.Width;
+            int nH = pic0.Height;
+            // El rectángulo que ocupará cada nuevo trozo
+            Rectangle rectDest = new Rectangle(0, 0, nW, nH);
+            // Estas variables se usan en el bucle
+            Bitmap bmpDest;
+            Graphics g;
+            Rectangle rectOrig;
+            //
+            // Array con los pictures que hay en el formulario
+            PictureBox[] trozos = { pic0, pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15};
+            // Para contar cada columna
+            int c = 0;
+            // La posición X e Y en la imagen original
+            int pX = 0, pY = 0;
+            for (int i = 0; i < trozos.Length; i++)
+            {
+                // El trozo de la imagen original
+                rectOrig = new Rectangle(pX, pY, tamTrozoW, tamTrozoH);
+                // La imagen de destino
+                bmpDest = new Bitmap(tamTrozoW, tamTrozoW);
+                g = Graphics.FromImage(bmpDest);
+                // Obtenemos un trozo de la imagen original
+                // y lo dibujamos en la imagen de destino
+                g.DrawImage(picMain.Image, rectDest, rectOrig, GraphicsUnit.Pixel);
+                // Asignamos la nueva imagen al picture correspondiente
+                trozos[vec[i]].Image = bmpDest;
+                c += 1;
+                pX += tamTrozoW;
+                // Cuando hayamos recorrido las columnas,
+                // pasamos a la siguiente fila
+                if (c >= columnas)
+                {
+                    c = 0;
+                    pX = 0;
+                    pY += tamTrozoH;
+                }
+            }
+        }
+        
+        public int GenerarNumero()
+        {
+            Random r = new Random();
+            return r.Next(0, 16);
+        }
+        
+        public void LlenarVector()
+        {
+            vec[0] = -1;
+            vec[1] = -2;
+            vec[2] = -3;
+            vec[3] = -4;
+            vec[4] = -5;
+            vec[5] = -6;
+            vec[6] = -7;
+            vec[7] = -8;
+            vec[8] = -9;
+            vec[9] = -10;
+            vec[10] = -11;
+            vec[11] = -12;
+            vec[12] = -13;
+            vec[13] = -14;
+            vec[14] = -15;
+            vec[15] = -16;
+
+            int i = 0;
+            
+            while (i < vec.Length)
+            {
+                int num = GenerarNumero();
+                if (vec.Where(r => r == num).DefaultIfEmpty(-1).Single() == -1)                
+                {
+                    vec[i] = num;
+                    i++;
+                }                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            for (int i = 0; i < vec.Length; i++)
+            {
+                MessageBox.Show(vec[i].ToString());
+            }
+          
+        }
+
+    }
+}
